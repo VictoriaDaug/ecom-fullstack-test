9928619b71f8a86a667df571ae7a17196c2e3096
Victoria Daug <victoria.daug@gmail.com>
2018-08-12 23:48:10 +0100

    Add font style

--- src/views/components/description-component/description.js
+++ src/views/components/description-component/description.js
@@ -3,11 +3,11 @@ import React from 'react';
 const ProductDescription = ({ title, description, priceLabel, currency, price }) => {
   const normalizedPrice = (price/100).toFixed(2);
   return(
-    <div>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <p>
-        {priceLabel}<span>{currency}{normalizedPrice}</span>
+    <div className='description'>
+      <h1 className='description-title'>{title}</h1>
+      <p className='description-text'>{description}</p>
+      <p className='description-price'>
+        {priceLabel}<span className='description-price_num'>{currency}{normalizedPrice}</span>
       </p>
     </div>
   );
