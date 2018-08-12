d176c0eba99477925a2fc9c2ecd1c89994948c95
Victoria Daug <victoria.daug@gmail.com>
2018-08-12 23:52:49 +0100

    Add styling button

--- src/views/components/button-component/button.js
+++ src/views/components/button-component/button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = ({ buttonLabel }) => (
-    <button>{buttonLabel}</button>
+    <button className='productButton-label'>{buttonLabel}</button>
 )
 
 export default Button;
