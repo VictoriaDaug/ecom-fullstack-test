521c3ac55d17e75c5168746a2bc180075a2828ef
Victoria Daug <victoria.daug@gmail.com>
2018-08-12 22:50:29 +0100

    Add picture component and test

--- src/views/components/picture-component/picture.js
+++ src/views/components/picture-component/picture.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const ProductPicture = ({ image, label }) => (
+const ProductPicture = ({ image, label }) => {
+
+  return(
     <div>
+      <img src={`/dist/${image.path}`} alt={image.alt} />
+      <div>{label}</div> 
     </div>
   )
+
+}
 
 export default ProductPicture;
--- src/views/components/picture-component/picture.test.js
+++ src/views/components/picture-component/picture.test.js
@@ -1,37 +1,19 @@
 import React from 'react';
 import Enzyme, {shallow, mount} from 'enzyme';
 
-import Product from './product';
+import ProductPicture from './picture';
 
-describe('<Product />', () => {
-    const props = [{
-        "title": "Retro Canvas",
-        "description": "A cool vintage look. For people. Places. And spaces.",
-        "image": {
-            "path": "/images/product.jpg",
-            "alt": "Retro Canvas"
-        },
-        "price": 500,
-        "currency": "£",
-        "priceLabel": "From",
-        "productLabel": "",
-        "cta": "Shop Now",
-        "ctaLink": "/random/link/to/no/where"
-    }, {
-        "title": "Desk Canvas",
-        "description": "Our smallest canvas for your big moments.",
-        "image": {
-            "path": "/images/product.jpg",
-            "alt": "Desk Canvas"
-        },
-        "price": 1250,
-        "currency": "£",
-        "priceLabel": "From",
-        "productLabel": "",
-        "cta": "Shop Now",
-        "ctaLink": "/random/link/to/no/where"
-    }]
-    it('renders without crashing', () => {
-        shallow(<Product data={props}/>);
+describe('<ProductPicture />', () => {
+    const image = {
+      "image": {
+          "path": "/images/product.jpg",
+          "alt": "Retro Canvas"
+      }
+    }
+    const label ={
+      "productLabel": ""
+    }
+    it('Renders without crashing', () => {
+        shallow(<ProductPicture image={image} label={label}/>);
     });
 });
