fece29bb32c97e9ae2f9a878d119116ef67ee1be
Victoria Daug <victoria.daug@gmail.com>
2018-08-12 23:07:37 +0100

    Add error page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Router, Route, hashHistory } from 'react-router';
 
 import HomePage from '../views/HomePage';
+import ErrorPage from '../views/ErrorPage';
 
 export default (
     <Router history={hashHistory}>
-        <Route path="/" component={HomePage}/>
+        <Route path="/" component={HomePage} />
+        <Route patch="*" component={ErrorPage} />
     </Router>
 );
---
+++ src/views/ErrorPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ErrorPage = () => (
+    <div>
+      <h1>404</h1>
+      <h3>Page Not Found</h3>
+    </div>
+  );
+export default ErrorPage;
