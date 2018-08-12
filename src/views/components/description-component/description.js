d08e3dfb8a9a948e23b200342ab27505745b5c29
Victoria Daug <victoria.daug@gmail.com>
2018-08-12 22:37:55 +0100

    Add description component

--- src/views/components/description-component/description.js
+++ src/views/components/description-component/description.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const ProductDescription = ({ title, description, priceLabel, currency, price }) => (
+const ProductDescription = ({ title, description, priceLabel, currency, price }) => {
+  const normalizedPrice = (price/100).toFixed(2);
+  return(
     <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <p>
+        {priceLabel}<span>{currency}{normalizedPrice}</span>
+      </p>
     </div>
-  )
+  );
+}
 
 export default ProductDescription;
